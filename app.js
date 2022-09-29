965afba0623844f943a955e7e31eeaa6609d920b
Rio Edwards <rioredwards@gmail.com>
2022-09-29 11:10:42 -0700

    Create List Items

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
+import { createItem } from './fetch-utils.js';
 
 /* Get DOM Elements */
+const addItemForm = document.getElementById('add-item-form');
+const deleteButton = document.getElementById('delete-button');
+const errorDisplay = document.getElementById('error-display');
+const itemList = document.getElementById('item-list');
 
 /* State */
+let items = [];
+let error = null;
 
 /* Events */
+addItemForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(addItemForm);
+    const newItem = {
+        item: formData.get('item'),
+        quantity: formData.get('quantity'),
+    };
+
+    // > Part A: Call the function to create a Item, passing in "newItem":
+    const response = await createItem(newItem);
+    error = response.error;
+    const item = response.data;
+
+    if (error) {
+        displayError();
+    } else {
+        items.push(item);
+        // displayItems();
+        addItemForm.reset();
+    }
+});
 
 /* Display Functions */
+function displayError() {
+    if (error) {
+        errorDisplay.textContent = error.message;
+    } else {
+        errorDisplay.textContent = '';
+    }
+}
